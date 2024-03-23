4aa71f19ead42e5ebfe887b524bf613917290d94
Phusit Somboonyingsuk <phusitsom@gmail.com>
2024-03-23 20:26:52 +0700

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -85,6 +85,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       themeMode: themeMode,
       theme: lightThemeData,
       darkTheme: darkThemeData,
